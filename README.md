a8793898dc42baf22b7d1a3206355aa1b628bde6
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2025-10-06 23:12:59 +0700

    update footer and add another notes

--- README.md
+++ README.md
@@ -1,15 +1,13 @@
-<div align="center">
-  <img src="./public/images/logo.png" alt="DevBook Logo" width="120" />
-
-</div>
-
 # devbook - an opinionated engineering handbook
+
+<div align="center">
+  <img src="/public/images/logo.png" alt="DevBook Logo" width="120" style="background: white; padding: 8px; border-radius: 8px;" />
+</div>
 
 ## motivation
 
 DevBook is a dynamic digital notebook where I document and share my ongoing exploration in the field of Software Engineering. This is more than just a collection of notes, it's a living archive of the skills, insights, and experiences I've gathered through internships, mentorships, college courses, and self-study.
 
 At its core, DevBook is my personal commitment to lifelong learning and the open sharing of knowledge. It is a space where I track my growth, organize complex concepts, and reflect on the lessons learned from real-world experiences. By sharing this journey publicly, I hope to inspire others to pursue their passions, embrace curiosity, and continuously improve themselves.
-
 
 Built with [Fumadocs](https://fumadocs.dev).
--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -7,17 +7,30 @@ export default function Layout({ children }: LayoutProps<'/'>) {
       {children}
       {/* Footer */}
       <footer className="mx-auto w-full max-w-2xl py-4">
-        <p className="text-center text-sm text-fd-muted-foreground">
-          built by{' '}
-          <a
-            href="https://x.com/arayyye"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="font-medium underline decoration-fd-muted-foreground/30 underline-offset-4 transition-colors hover:text-fd-foreground hover:decoration-fd-foreground/50"
-          >
-            arayyye
-          </a>
-        </p>
+        <div className="flex flex-col items-center justify-center">
+          <p className="text-center text-sm text-fd-foreground">
+            built by{' '}
+            <a
+              href="https://x.com/arayyye"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="font-medium underline decoration-fd-muted-foreground/30 underline-offset-4 transition-colors hover:text-fd-foreground hover:decoration-fd-foreground/50"
+            >
+              arayyye
+            </a>
+          </p>
+          <p className="text-center text-sm text-fd-foreground">
+            the code is available on{' '}
+            <a
+              href="https://github.com/ahmadrafidev/handbook"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="font-medium underline decoration-fd-muted-foreground/30 underline-offset-4 transition-colors hover:text-fd-foreground hover:decoration-fd-foreground/50"
+            >
+              GitHub
+            </a>
+          </p>
+        </div>
       </footer>  
     </HomeLayout>
   );
